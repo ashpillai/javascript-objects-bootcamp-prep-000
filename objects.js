b76585c01e58b4dcc78ec0ba9cd3e1dafbc3d91c
pillai.ashwini@gmail.com <pillai.ashwini@gmail.com>
2018-07-19 02:29:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,10 +5,10 @@ var playlist = {
 };
 
 function updatePlaylist(obj, artist, song) {
-  obj[artist] = song
-  return obj
+  obj[artist] = song;
+  return obj;
 }
 
 function removeFromPlaylist(obj, artist) {
-  delete obj[artist]
+  delete obj[artist];
 }
